602e5d86d9c813c444cb5b2daae7b3833ec5a538
dramforever <dramforever@live.com>
2021-04-02 15:50:51 +0800

    Update

--- flake.nix
+++ flake.nix
@@ -5,19 +5,27 @@
     checks.x86_64-linux.nix = nix.packages.x86_64-linux.nix.overrideAttrs (old: {
       checkPhase = ''
         runHook preCheck
+        set +e
         for i in $(seq 100); do
-          echo Running check, round $i
+          echo === Running check, round $i
           make check
         done
+        echo === Final check
+        set -e
+        make check
         runHook postCheck
       '';
 
       installCheckPhase = ''
         runHook preInstallCheck
+        set +e
         for i in $(seq 100); do
           echo Running installcheck, round $i
           make installcheck
         done
+        set -e
+        echo === Final installcheck
+        make installcheck
         runHook postInstallCheck
       '';
     });
