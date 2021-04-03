be5bde3c49968b1cb95b5e1755b6e98c674062c2
dramforever <dramforever@live.com>
2021-04-03 21:33:24 +0800

    Don't rerun make check

--- flake.nix
+++ flake.nix
@@ -3,19 +3,6 @@
 
   outputs = { self, nix }: {
     checks.x86_64-linux.nix = nix.packages.x86_64-linux.nix.overrideAttrs (old: {
-      checkPhase = ''
-        runHook preCheck
-        set +e
-        for i in $(seq 100); do
-          echo === Running check, round $i
-          make check
-        done
-        echo === Final check
-        set -e
-        make check
-        runHook postCheck
-      '';
-
       installCheckPhase = ''
         runHook preInstallCheck
         set +e
